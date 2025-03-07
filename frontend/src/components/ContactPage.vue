12f515139243afa15290946e2eee43b0fb7022ee
keanan-oliver <keananwork21@gmail.com>
2025-03-07 08:05:06 +0200

    email validation - keanan

--- frontend/src/components/ContactPage.vue
+++ frontend/src/components/ContactPage.vue
@@ -1,22 +1,27 @@
 <template>
+  <div class="contact-container">
     <h1>Contact Page</h1>
 
     <label>Name: </label>
-    <input type="text" v-model="name">
+    <input type="text" v-model="name" />
+    <div v-if="errors.name" class="error">{{ errors.name }}</div>
 
-    <br>
+    <br />
 
     <label>Email: </label>
-    <input type="email" v-model="email">
+    <input type="email" v-model="email" />
+    <div v-if="errors.email" class="error">{{ errors.email }}</div>
 
-    <br>
+    <br />
 
     <label>Message</label>
     <textarea type="text" v-model="message"></textarea>
+    <div v-if="errors.message" class="error">{{ errors.message }}</div>
 
-    <br>
+    <br />
 
     <button @click="sendContact">Send</button>
+  </div>
 </template>
 
 <script>
@@ -27,8 +32,28 @@ export default {
         const name = ref('')
         const email = ref('')
         const message = ref('')
+        const errors = ref({})
 
         const sendContact = async () => {
+
+          errors.value = {}; // Reset errors
+
+            if (!name.value) {
+                errors.value.name = 'Name is required.';
+            }
+            if (!email.value) {
+                errors.value.email = 'Email is required.';
+            } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
+                errors.value.email = 'Invalid email format.';
+            }
+            if (!message.value) {
+                errors.value.message = 'Message is required.';
+            }
+
+            if (Object.keys(errors.value).length > 0) {
+                return; // Stop if there are errors
+            }
+
       try {
         const response = await fetch('http://localhost:3000/api/contact', {
           method: 'POST',
@@ -56,7 +81,8 @@ export default {
       name,
       email,
       message,
-      sendContact
+      sendContact,
+      errors
     };
     
 }
@@ -65,10 +91,43 @@ export default {
 </script>
 
 <style>
+.contact-container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 20px;
+  font-family: Arial, Helvetica, sans-serif;
+ 
+}
+
+.contact-container input{
+  width: 300px;
+  border-radius: 5px;
+}
+
+.contact-container textarea{
+  width: 300px;
+  border-radius: 10px;
+}
+
+.contact-container button{
+  width: 200px;
+  background: red;
+  color: white;
+  height: 30px;
+  border-radius: 30px;
+  cursor: pointer;
+}
+
 textarea{
     width: 300px;
     min-height: 100px;
     resize: vertical;
+}
+
+.error {
+    color: red;
+    font-size: 12px;
 }
 
 </style>
